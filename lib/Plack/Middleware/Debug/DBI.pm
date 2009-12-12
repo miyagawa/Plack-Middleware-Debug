b49acc940587dd620c80d9d1c270a26162c0e865
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-12-12 05:49:03 -0800

    Added raw DBI tracing panel

---
+++ lib/Plack/Middleware/Debug/DBI.pm
@@ -0,0 +1,69 @@
+package Plack::Middleware::Debug::DBI;
+use 5.008;
+use strict;
+use warnings;
+use Template;
+use Plack::Util::Accessor qw(renderer);
+use parent qw(Plack::Middleware::Debug::Base);
+
+sub TEMPLATE {
+    <<'EOTMPL' }
+<table>
+    <thead>
+        <tr>
+            <th>Trace</th>
+        </tr>
+    </thead>
+    <tbody>
+            <tr class="[% cycle('djDebugEven' 'djDebugOdd') %]">
+                <td><pre>[% dump | html %]</pre></td>
+            </tr>
+    </tbody>
+</table>
+EOTMPL
+
+sub init {
+    my $self = shift;
+    $self->renderer(Template->new);
+}
+sub nav_title { 'DBI' }
+
+sub process_request {
+    my ($self, $env) = @_;
+    if (defined &DBI::trace) {
+        $env->{'plack.debug.dbi.trace'} = DBI->trace;
+        open my $fh, ">", \my $output;
+        DBI->trace("1,SQL", $fh);
+        $env->{'plack.debug.dbi.output'} = \$output;
+    }
+}
+
+sub process_response {
+    my ($self, $res, $env) = @_;
+
+    my $content;
+    if (defined(my $trace = $env->{'plack.debug.dbi.trace'})) {
+        DBI->trace($trace); # reset
+        my $dump = $env->{'plack.debug.dbi.output'};
+        my $template = $self->TEMPLATE;
+        my $vars     = {
+            $self->renderer_vars,
+            dump => $$dump,
+        };
+        $self->renderer->process(\$template, $vars, \$content)
+            || die $self->renderer->error;
+    }
+    $self->content($content);
+}
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Debug::DBI - DBI trace panel
+
+=head1 SEE ALSO
+
+L<Plack::Middleware::Debug>
+
+=cut
