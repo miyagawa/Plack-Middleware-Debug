77d4cde3b0ec741ff8dc41b4235f4a619b9dd127
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-09-06 11:37:31 -0700

    0.15

--- lib/Plack/Middleware/Debug/ModuleVersions.pm
+++ lib/Plack/Middleware/Debug/ModuleVersions.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Module::Versions;
 use parent qw(Plack::Middleware::Debug::Base);
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 sub run {
     my ($self, $env, $panel) = @_;
